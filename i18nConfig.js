e60f24f7491c2741d50c221a0210d6ce4c8428e3
JayJ104 <123anjalijain@gmail.com>
2024-01-24 00:27:27 -0800

    Revert "config (not tested yet)"
    This reverts commit a034a1732e00a3c4359fb0356db88d240aa090a9.

--- i18nConfig.js
+++
@@ -1,9 +0,0 @@
-// sub-path routing
-// https://nextjs.org/docs/pages/building-your-application/routing/internationalization 
-const i18nConfig = {
-      locales: ['en', 'fr', 'zh'],
-      defaultLocale: 'en',
-      localeDetector: false,
-};
-    
-module.exports = i18nConfig;
